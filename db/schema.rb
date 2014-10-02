f75d5789406b082797e022773226abadb4da2634
Rodrigo <rodrigo.naranjo@alumnos.usm.cl>
2014-10-02 18:26:31 -0300

    No funcionaba nada al final

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140930044226) do
+ActiveRecord::Schema.define(version: 20141002053451) do
+
+  create_table "admins", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "clients", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "nombre"
