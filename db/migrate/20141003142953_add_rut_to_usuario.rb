ce6f1f5409c2c185324cb0fb41a0842c9fd7a929
Franz Wompner <franz.wompner@alumnos.usm.cl>
2014-11-19 16:32:23 -0300

    Remove troublesome migrations; @cuenta.pago must be nil to allow destruction

--- db/migrate/20141003142953_add_rut_to_usuario.rb
+++
@@ -1,5 +0,0 @@
-class AddRutToUsuario < ActiveRecord::Migration
-  def change
-    add_column :usuarios, :rut, :string
-  end
-end
--- db/migrate/20141107151616_change_table_name.rb
+++
@@ -1,5 +0,0 @@
-class ChangeTableName < ActiveRecord::Migration
-  def change
-  	rename_table :usuarios, :users
-  end
-end
